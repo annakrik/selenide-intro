9582043f986c6b69561c840a0ce20e835a5910f7
Anna Krykshtulis <ulisanna@gmail.com>
2020-07-09 19:35:49 +0300

    removed cssClass, updated selectors

--- src/test/java/org/an/selenideintro/TodoMvcTest.java
+++ src/test/java/org/an/selenideintro/TodoMvcTest.java
@@ -3,7 +3,7 @@ package org.an.selenideintro;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
-import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TodoMvcTest {
@@ -16,11 +16,10 @@ public class TodoMvcTest {
         element("#new-todo").setValue("c").pressEnter();
         elements("#todo-list>li").shouldHave(exactTexts("a", "b", "c"));
 
-        element("li:nth-child(2) .toggle").click();
+        element("#todo-list li:nth-child(2) .toggle").click();
 
-        element("li:nth-child(2)").shouldHave(cssClass("completed"));
-        element("li:nth-child(1)").shouldNotHave(cssClass("completed"));
-        element("li:nth-child(3)").shouldNotHave(cssClass("completed"));
+        element("#todo-list li.completed").shouldHave(exactText("b"));
+        elements("#todo-list li:not(.completed)").shouldHave(exactTexts("a","c"));
     }
 
 
